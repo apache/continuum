1ebfbd0c9a1c07887c26c5e37c902936b1b77604
David Blevins <dblevins@apache.org>
2005-11-05 01:26:00 +0000

    Basic structure for gbuild
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gbuild/trunk@330951 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/geronimo/gbuild/App.java
@@ -0,0 +1,13 @@
+package org.apache.geronimo.gbuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
