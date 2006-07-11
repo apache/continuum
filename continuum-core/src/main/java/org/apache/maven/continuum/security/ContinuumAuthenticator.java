fe7ad19703f774e492ff15e515e0dab8dbb7fc8f
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-11 10:20:28 +0000

    [CONTINUUM-764] Added continuum-security-acegi module
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/branches/continuum-acegi@420787 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-core/src/main/java/org/apache/maven/continuum/security/ContinuumAuthenticator.java
+++ continuum-core/src/main/java/org/apache/maven/continuum/security/ContinuumAuthenticator.java
@@ -17,18 +17,9 @@ package org.apache.maven.continuum.security;
  *
  */
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
-import org.acegisecurity.GrantedAuthority;
-import org.acegisecurity.GrantedAuthorityImpl;
-import org.acegisecurity.userdetails.User;
-import org.acegisecurity.userdetails.UserDetails;
-import org.acegisecurity.userdetails.UserDetailsService;
-import org.acegisecurity.userdetails.UsernameNotFoundException;
 import org.apache.maven.continuum.model.system.ContinuumUser;
-import org.apache.maven.continuum.model.system.Permission;
 import org.apache.maven.continuum.store.ContinuumStore;
 import org.apache.maven.continuum.store.ContinuumStoreException;
 import org.codehaus.plexus.security.Authentication;
@@ -36,8 +27,6 @@ import org.codehaus.plexus.security.Authenticator;
 import org.codehaus.plexus.security.exception.AuthenticationException;
 import org.codehaus.plexus.security.exception.UnauthorizedException;
 import org.codehaus.plexus.security.exception.UnknownEntityException;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataAccessResourceFailureException;
 
 /**
  * TODO: Move this to o.a.m.c.security once plexus-security doesn't depend on plexus-summit.
@@ -46,7 +35,7 @@ import org.springframework.dao.DataAccessResourceFailureException;
  * @version $Id$
  */
 public class ContinuumAuthenticator
-    implements Authenticator, UserDetailsService
+    implements Authenticator
 {
     /**
      * @plexus.requirement
@@ -82,25 +71,6 @@ public class ContinuumAuthenticator
         return null;
     }
 
-    public UserDetails loadUserByUsername( String username )
-        throws UsernameNotFoundException, DataAccessException
-    {
-        ContinuumUser user;
-        try
-        {
-            user = store.getUserByUsername( username );
-        }
-        catch ( ContinuumStoreException e )
-        {
-            throw new DataAccessResourceFailureException( e.getMessage(), e );
-        }
-        if ( user == null )
-        {
-            throw new UsernameNotFoundException( "Could not find user: " + username );
-        }
-        return getUserDetails( user );
-    }
-
     public Authentication getAnonymousEntity()
     {
         throw new RuntimeException( "Not implemented" );
@@ -121,35 +91,5 @@ public class ContinuumAuthenticator
         {
             throw new AuthenticationException( "Error while retreiving user.", e );
         }
-    }
-    
-    /**
-     * Convert a Continuum user into a Acegi user
-     * 
-     * @param user the continuum user loaded from DB
-     * @return the Acegi user
-     */
-    private UserDetails getUserDetails( ContinuumUser user )
-    {
-        List permissions = user.getGroup().getPermissions();
-
-        GrantedAuthority[] grantedAuthorities = new GrantedAuthority[permissions.size()];
-        int i = 0;
-        Iterator it = permissions.iterator();
-        while ( it.hasNext() )
-        {
-            Permission permission = (Permission) it.next();
-            grantedAuthorities[i] = new GrantedAuthorityImpl( permission.getName() );
-            i++;
-        }
-        boolean enabled = true;
-        boolean accountNonExpired = true;
-        boolean credentialsNonExpired = true;
-        boolean accountNonLocked = true;
-
-        UserDetails userDetails = new User( user.getUsername(), user.getPassword(), enabled, accountNonExpired,
-                                            credentialsNonExpired, accountNonLocked, grantedAuthorities );
-
-        return userDetails;
     }
 }
---
+++ continuum-security/continuum-security-acegi/src/main/java/org/apache/maven/continuum/security/acegi/ContinuumUserDetailsService.java
@@ -0,0 +1,108 @@
+package org.apache.maven.continuum.security.acegi;
+
+/*
+ * Copyright 2005-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
+import org.acegisecurity.userdetails.User;
+import org.acegisecurity.userdetails.UserDetails;
+import org.acegisecurity.userdetails.UserDetailsService;
+import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.apache.maven.continuum.model.system.ContinuumUser;
+import org.apache.maven.continuum.model.system.Permission;
+import org.apache.maven.continuum.store.ContinuumStore;
+import org.apache.maven.continuum.store.ContinuumStoreException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+/**
+ * Acegi {@link UserDetailsService} that loads user info from Continuum database.
+ *
+ * @author <a href="mailto:carlos@apache.org">Carlos Sanchez</a>
+ * @author Henry Isidro
+ * @version $Id$
+ */
+public class ContinuumUserDetailsService
+    implements UserDetailsService
+{
+    /**
+     * @plexus.requirement
+     */
+    private ContinuumStore store;
+
+    public ContinuumUserDetailsService()
+    {
+    }
+
+    public UserDetails loadUserByUsername( String username )
+        throws UsernameNotFoundException, DataAccessException
+    {
+        ContinuumUser user;
+        try
+        {
+            user = store.getUserByUsername( username );
+        }
+        catch ( ContinuumStoreException e )
+        {
+            throw new DataAccessResourceFailureException( e.getMessage(), e );
+        }
+        if ( user == null )
+        {
+            throw new UsernameNotFoundException( "Could not find user: " + username );
+        }
+        return getUserDetails( user );
+    }
+
+    /**
+     * Convert a Continuum user into a Acegi user
+     * 
+     * @param user the continuum user loaded from DB
+     * @return the Acegi user
+     */
+    private UserDetails getUserDetails( ContinuumUser user )
+    {
+        List permissions = user.getGroup().getPermissions();
+
+        GrantedAuthority[] grantedAuthorities = new GrantedAuthority[permissions.size()];
+        int i = 0;
+        Iterator it = permissions.iterator();
+        while ( it.hasNext() )
+        {
+            Permission permission = (Permission) it.next();
+            grantedAuthorities[i] = new GrantedAuthorityImpl( permission.getName() );
+            i++;
+        }
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
+        UserDetails userDetails = new User( user.getUsername(), user.getPassword(), enabled, accountNonExpired,
+                                            credentialsNonExpired, accountNonLocked, grantedAuthorities );
+
+        return userDetails;
+    }
+
+    /**
+    *TODO: clean up ContinuumAuthenticator
+    *TODO: convert Acegi user into Continuum user?
+    */
+}
