ce7a4c89eeb16dca8f2bc0f7df9b27007b0f1a7a
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-08-19 02:20:02 +0000

    [CONTINUUM-822] First pass to test ACL database creation
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/branches/continuum-acegi@432782 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-security/continuum-security-acegi/src/main/resources/org/apache/maven/continuum/security/acegi/acl/acegi-acl-derby.sql
+++ continuum-security/continuum-security-acegi/src/main/resources/org/apache/maven/continuum/security/acegi/acl/acegi-acl-derby.sql
@@ -1,15 +1,13 @@
-DROP TABLE acl_object_identity;
+CREATE SCHEMA sa;
 
 CREATE TABLE acl_object_identity (
-     id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT acl_object_identity_PK PRIMARY KEY,
+     id INTEGER NOT NULL CONSTRAINT acl_object_identity_PK PRIMARY KEY,
      object_identity VARCHAR(250) NOT NULL,
      parent_object INTEGER,
      acl_class VARCHAR(250) NOT NULL,
      CONSTRAINT unique_object_identity UNIQUE(object_identity),
      FOREIGN KEY (parent_object) REFERENCES acl_object_identity(id)
 );
-
-DROP TABLE acl_object_identity;
 
 CREATE TABLE acl_permission (
      id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT acl_permission_PK PRIMARY KEY,
@@ -20,15 +18,15 @@ CREATE TABLE acl_permission (
      FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity(id)
 );
 
-INSERT INTO acl_object_identity VALUES (1, 'corp.DomainObject:1', null, 'org.acegisecurity.acl.basic.SimpleAclEntry');
-INSERT INTO acl_object_identity VALUES (2, 'corp.DomainObject:2', 1, 'org.acegisecurity.acl.basic.SimpleAclEntry');
-INSERT INTO acl_object_identity VALUES (3, 'corp.DomainObject:3', 1, 'org.acegisecurity.acl.basic.SimpleAclEntry');
-INSERT INTO acl_object_identity VALUES (4, 'corp.DomainObject:4', 1, 'org.acegisecurity.acl.basic.SimpleAclEntry');
-INSERT INTO acl_object_identity VALUES (5, 'corp.DomainObject:5', 3, 'org.acegisecurity.acl.basic.SimpleAclEntry');
-INSERT INTO acl_object_identity VALUES (6, 'corp.DomainObject:6', 3, 'org.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (1, 'org.apache.maven.continuum.model.project.Project:1', null, 'org.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (2, 'org.apache.maven.continuum.model.project.Project:2', 1, 'org.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (3, 'org.apache.maven.continuum.model.project.Project:3', 1, 'org.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (4, 'org.apache.maven.continuum.model.project.Project:4', 1, 'org.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (5, 'org.apache.maven.continuum.model.project.Project:5', 3, 'org.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (6, 'org.apache.maven.continuum.model.project.Project:6', 3, 'org.acegisecurity.acl.basic.SimpleAclEntry');
 
-INSERT INTO acl_permission VALUES (null, 1, 'ROLE_SUPERVISOR', 1);
-INSERT INTO acl_permission VALUES (null, 2, 'ROLE_SUPERVISOR', 0);
-INSERT INTO acl_permission VALUES (null, 2, 'marissa', 2);
-INSERT INTO acl_permission VALUES (null, 3, 'scott', 14);
-INSERT INTO acl_permission VALUES (null, 6, 'scott', 1);
+INSERT INTO acl_permission (acl_object_identity, recipient, mask) VALUES (1, 'ROLE_ADMIN', 1);
+INSERT INTO acl_permission (acl_object_identity, recipient, mask) VALUES (2, 'ROLE_ADMIN', 0);
+INSERT INTO acl_permission (acl_object_identity, recipient, mask) VALUES (2, 'marissa', 2);
+INSERT INTO acl_permission (acl_object_identity, recipient, mask) VALUES (3, 'scott', 14);
+INSERT INTO acl_permission (acl_object_identity, recipient, mask) VALUES (6, 'scott', 1);
