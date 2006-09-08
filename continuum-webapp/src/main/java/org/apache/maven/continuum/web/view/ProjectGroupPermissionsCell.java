3b5b937baa333db7e3d215337ee113bd3ea70e2f
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-09-08 01:29:35 +0000

    Reformat
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/branches/continuum-acegi@441321 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-webapp/src/main/java/org/apache/maven/continuum/web/view/ProjectGroupPermissionsCell.java
+++ continuum-webapp/src/main/java/org/apache/maven/continuum/web/view/ProjectGroupPermissionsCell.java
@@ -1,31 +1,58 @@
 package org.apache.maven.continuum.web.view;
 
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.extremecomponents.table.bean.Column;
-import org.extremecomponents.table.cell.DisplayCell;
-import org.extremecomponents.table.core.TableModel;
 import org.extremecomponents.table.cell.Cell;
+import org.extremecomponents.table.core.TableModel;
 import org.extremecomponents.table.view.html.ColumnBuilder;
 
-public class ProjectGroupPermissionsCell implements Cell
+/**
+ * 
+ * @author <a href="mailto:hisidro@exist.com">Henry Isidro</a>
+ * @version $Id$
+ */
+public class ProjectGroupPermissionsCell
+    implements Cell
 {
-        public String getExportDisplay(TableModel model, Column column) {
-            return null;
+    public String getExportDisplay( TableModel model, Column column )
+    {
+        return null;
+    }
+
+    public String getHtmlDisplay( TableModel model, Column column )
+    {
+        ColumnBuilder columnBuilder = new ColumnBuilder( column );
+
+        columnBuilder.tdStart();
+
+        try
+        {
+            columnBuilder.getHtmlBuilder().input( "checkbox" );
+            columnBuilder.getHtmlBuilder().checked();
+            columnBuilder.getHtmlBuilder().xclose();
+        }
+        catch ( Exception e )
+        {
         }
 
-        public String getHtmlDisplay(TableModel model, Column column) {
-            ColumnBuilder columnBuilder = new ColumnBuilder(column);
-            
-            columnBuilder.tdStart();
-            
-            try {
-                columnBuilder.getHtmlBuilder().input("checkbox");
-                columnBuilder.getHtmlBuilder().checked();
-                columnBuilder.getHtmlBuilder().xclose();
-            } catch (Exception e) {}
-            
-            columnBuilder.tdEnd();
-            
-            return columnBuilder.toString();
-        }
+        columnBuilder.tdEnd();
+
+        return columnBuilder.toString();
+    }
 
 }
