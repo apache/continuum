4cea07fe2a4bd0e41796d48185b0503cdfefddf8
Brett Leslie Porter <brett@apache.org>
2007-06-13 15:33:48 +0000

    restore license on OID
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/continuum/trunk@546924 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-data-management/data-management-jdo/src/main/java/org/jpox/store/OID.java
+++ continuum-data-management/data-management-jdo/src/main/java/org/jpox/store/OID.java
@@ -1,23 +1,28 @@
-package org.jpox.store;
+/**********************************************************************
+ Copyright (c) 2002 Kelly Grizzle (TJDO) and others. All rights reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+
+ Contributors:
+ 2003 Erik Bengtson - Refactored OID
+ 2003 Andy Jefferson - fixed OID(String)
+ 2003 Andy Jefferson - coding standards
+ 2004 Andy Jefferson - fixes to allow full use of long or String OIDs
+ 2005 Erik Bengtson - removed oidType
+ 2007 Brett Porter - changed hashcode algorithm to avoid collisions (see CORE-3297)
+ ...
+ **********************************************************************/
+package org.jpox.store;
 
 import org.jpox.ClassNameConstants;
 import org.jpox.util.Localiser;
