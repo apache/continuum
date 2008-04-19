c60ec38c0c93121ac617f9e15d03bdaad3bef891
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-04-19 03:17:11 +0000

    [CONTINUUM-1731] Add some commands to use a chroot jail in the build executor, keeping previous functionality unmodified
    git-svn-id: https://svn.eu.apache.org/repos/asf/continuum/trunk@649750 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-commons/src/main/java/org/apache/maven/continuum/utils/ChrootJailWorkingDirectoryService.java
@@ -0,0 +1,76 @@
+package org.apache.maven.continuum.utils;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.continuum.configuration.ConfigurationService;
+import org.apache.maven.continuum.model.project.Project;
+import org.apache.maven.continuum.model.project.ProjectGroup;
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+
+/**
+ * @author <a href="mailto:carlos@apache.org">Carlos Sanchez</a>
+ * @version $Id$
+ * @plexus.component role="org.apache.maven.continuum.utils.WorkingDirectoryService" role-hint="chrootJail"
+ */
+public class ChrootJailWorkingDirectoryService
+    extends AbstractLogEnabled
+    implements WorkingDirectoryService
+{
+    /**
+     * @plexus.requirement
+     */
+    private ConfigurationService configurationService;
+
+    /**
+     * @plexus.configuration
+     */
+    private File chrootJailDirectory;
+
+    public void setConfigurationService( ConfigurationService configurationService )
+    {
+        this.configurationService = configurationService;
+    }
+
+    public ConfigurationService getConfigurationService()
+    {
+        return configurationService;
+    }
+
+    public void setChrootJailDirectory( File chrootJailDirectory )
+    {
+        this.chrootJailDirectory = chrootJailDirectory;
+    }
+
+    public File getChrootJailDirectory()
+    {
+        return chrootJailDirectory;
+    }
+
+    public File getWorkingDirectory( Project project )
+    {
+        ProjectGroup projectGroup = project.getProjectGroup();
+
+        File f = new File( getChrootJailDirectory(), projectGroup.getGroupId() );
+        f = new File( f, getConfigurationService().getWorkingDirectory().getPath() );
+        return new File( f, Integer.toString( project.getId() ) );
+    }
+}
