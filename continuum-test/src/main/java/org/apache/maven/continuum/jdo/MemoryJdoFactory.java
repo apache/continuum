7468c9118a3b0be707e11fd3caf36d12863cbd7f
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-04-30 03:39:58 +0000

    Configure the jdo factory in Spring
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@652244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-test/src/main/java/org/apache/maven/continuum/jdo/MemoryJdoFactory.java
@@ -0,0 +1,19 @@
+package org.apache.maven.continuum.jdo;
+
+import java.util.Properties;
+
+import org.codehaus.plexus.jdo.DefaultConfigurableJdoFactory;
+
+public class MemoryJdoFactory
+    extends DefaultConfigurableJdoFactory
+{
+    public Properties getOtherProperties()
+    {
+        return otherProperties;
+    }
+
+    public void setOtherProperties( Properties otherProperties )
+    {
+        this.otherProperties = otherProperties;
+    }
+}
