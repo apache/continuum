83645a4627218f3dbba98d7f6a26ea53cbe83f89
Oliver Lamy <olamy@apache.org>
2008-05-11 22:24:23 +0000

    [CONTINUUM-1742] Duplicate installations (environment variables) in Profile are accepted
    git-svn-id: https://svn.eu.apache.org/repos/asf/continuum/trunk@655380 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/admin/ProfileAction.java
+++ continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/admin/ProfileAction.java
@@ -38,24 +38,6 @@ import org.codehaus.plexus.redback.xwork.interceptor.SecureActionException;
 
 import com.opensymphony.xwork.Preparable;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 /**
  * @author <a href="mailto:olamy@codehaus.org">olamy</a>
  * @version $Id$
@@ -201,12 +183,12 @@ public class ProfileAction
     // Webwork setter/getter
     // -------------------------------------------------------
 
-    public List getProfiles()
+    public List<Profile> getProfiles()
     {
         return profiles;
     }
 
-    public void setProfiles( List profiles )
+    public void setProfiles( List<Profile> profiles )
     {
         this.profiles = profiles;
     }
@@ -227,6 +209,11 @@ public class ProfileAction
         if ( this.allInstallations == null )
         {
             this.allInstallations = installationService.getAllInstallations();
+        }
+        // CONTINUUM-1742 (olamy) don't display already attached en var
+        if (this.profile != null)
+        {
+            this.allInstallations.removeAll( this.profile.getEnvironmentVariables() );
         }
         return allInstallations;
     }
