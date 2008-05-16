9a3b70f242cd88102efd180e33c70026915d7892
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-05-16 01:33:27 +0000

    Rename test to avoid confusion about it's abstractness
    git-svn-id: https://svn.eu.apache.org/repos/asf/continuum/trunk@656919 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-core/src/test/java/org/apache/maven/continuum/execution/ContinuumBuildExecutorTest.java
+++ continuum-core/src/test/java/org/apache/maven/continuum/execution/ContinuumBuildExecutorTest.java
@@ -1,5 +1,24 @@
 package org.apache.maven.continuum.execution;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +36,9 @@ import org.apache.maven.continuum.utils.shell.ShellCommandHelper;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.logging.console.ConsoleLogger;
 import org.jmock.Expectations;
-import org.jmock.Mock;
 import org.jmock.Mockery;
-import org.jmock.core.Constraint;
 
-public class AbstractContinuumBuildExecutorTest
+public class ContinuumBuildExecutorTest
     extends TestCase
 {
     protected AbstractBuildExecutor executor = new BuildExecutorStub();
