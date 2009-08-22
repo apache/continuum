1e1a2d8d68d999ac4f03d231841430d497c3d799
Maria Catherine R. Tan <ctan@apache.org>
2009-08-22 10:54:22 +0000

    [CONTINUUM-2141] upgrade modello to 1.1 merge -r806833:806834 of 1.3.x branch
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@806837 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-data-management/continuum-legacy/src/main/java/org/apache/maven/continuum/model/project/v1_0_9/ContinuumModelloMetadata.java
@@ -0,0 +1,16 @@
+package org.apache.maven.continuum.model.project.v1_0_9;
+
+public class ContinuumModelloMetadata
+{
+    private String modelVersion;
+
+    public String getModelVersion()
+    {
+        return modelVersion;
+    }
+
+    public void setModelVersion( String modelVersion )
+    {
+        this.modelVersion = modelVersion;
+    }
+}
