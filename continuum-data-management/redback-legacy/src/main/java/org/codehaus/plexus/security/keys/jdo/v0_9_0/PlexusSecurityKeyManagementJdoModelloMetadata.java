3ccb2cb4a842f5a2a20223d97bc48a71773f8c1d
Maria Catherine R. Tan <ctan@apache.org>
2009-08-22 10:30:16 +0000

    [CONTINUUM-2141] upgrade modello to 1.1
    git-svn-id: https://svn.apache.org/repos/asf/continuum/branches/continuum-1.3.x@806834 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-data-management/redback-legacy/src/main/java/org/codehaus/plexus/security/keys/jdo/v0_9_0/PlexusSecurityKeyManagementJdoModelloMetadata.java
@@ -0,0 +1,16 @@
+package org.codehaus.plexus.security.keys.jdo.v0_9_0;
+
+public class PlexusSecurityKeyManagementJdoModelloMetadata
+{
+    private String modelVersion;
+
+    public String getModelVersion()
+    {
+        return modelVersion;
+    }
+
+    public void setModelVersion( String modelVersion )
+    {
+        this.modelVersion = modelVersion;
+    }
+}
