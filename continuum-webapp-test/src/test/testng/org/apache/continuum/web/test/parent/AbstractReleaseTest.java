6edb394c7a73fa8a0e9228633617867cb5be2d78
Maria Catherine R. Tan <ctan@apache.org>
2010-02-10 02:03:10 +0000

    prevent selenium tests from running infinitely when release failed
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@908316 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-webapp-test/src/test/testng/org/apache/continuum/web/test/parent/AbstractReleaseTest.java
+++ continuum-webapp-test/src/test/testng/org/apache/continuum/web/test/parent/AbstractReleaseTest.java
@@ -111,11 +111,19 @@ public abstract class AbstractReleaseTest
     public void assertRelease( boolean success )
         throws Exception
     {
+        int currentIt = 1;
+        int maxIt = 20;
+
         while ( !isButtonWithValuePresent( "Done" ) && !isTextPresent( "Release Error" ) )
         {
             Thread.sleep( 10000 );
+            if ( currentIt > maxIt )
+            {
+                Assert.fail( "Timeout, Can't release project" );
+            }
+            currentIt++;
         }
-    
+
         assertButtonWithValuePresent( "Rollback changes" );
     
         if ( success )
