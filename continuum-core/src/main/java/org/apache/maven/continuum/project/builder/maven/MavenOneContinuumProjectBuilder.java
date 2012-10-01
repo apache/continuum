877a082ba0d2489b7f77de62d883e801311acbfb
Brett Porter <brett@apache.org>
2012-10-01 06:58:57 +0000

    [CONTINUUM-2563] correct equality condition to create group when it doesn't exist in the request
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@1392223 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-core/src/main/java/org/apache/maven/continuum/project/builder/maven/MavenOneContinuumProjectBuilder.java
+++ continuum-core/src/main/java/org/apache/maven/continuum/project/builder/maven/MavenOneContinuumProjectBuilder.java
@@ -173,7 +173,7 @@ public class MavenOneContinuumProjectBuilder
             }
         }
 
-        if ( projectGroup != null )
+        if ( projectGroup == null )
         {
             projectGroup = new ProjectGroup();
 
