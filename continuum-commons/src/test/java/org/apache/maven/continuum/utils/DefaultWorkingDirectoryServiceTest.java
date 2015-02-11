297575286ed37540c2f901635cedd82c6a71c6b6
Brent Atkinson <batkinson@apache.org>
2015-02-11 21:27:11 +0000

    Migrated continuum-commons from jmock to mockito.
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@1659078 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-commons/src/test/java/org/apache/maven/continuum/utils/DefaultWorkingDirectoryServiceTest.java
+++ continuum-commons/src/test/java/org/apache/maven/continuum/utils/DefaultWorkingDirectoryServiceTest.java
@@ -22,13 +22,12 @@ package org.apache.maven.continuum.utils;
 import org.apache.maven.continuum.configuration.ConfigurationService;
 import org.apache.maven.continuum.model.project.Project;
 import org.codehaus.plexus.spring.PlexusInSpringTestCase;
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit3.JUnit3Mockery;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author <a href="mailto:oching@apache.org">Maria Odea Ching</a>
@@ -38,21 +37,21 @@ public class DefaultWorkingDirectoryServiceTest
 {
     private DefaultWorkingDirectoryService workingDirectoryService;
 
-    private Mockery context;
-
     private ConfigurationService configurationService;
+
+    private File baseWorkingDirectory;
 
     public void setUp()
         throws Exception
     {
         super.setUp();
 
-        context = new JUnit3Mockery();
+        configurationService = mock( ConfigurationService.class );
 
-        configurationService = context.mock( ConfigurationService.class );
+        baseWorkingDirectory = new File( getBasedir(), "target" + File.separator + "working-directory" );
+        when( configurationService.getWorkingDirectory() ).thenReturn( baseWorkingDirectory );
 
         workingDirectoryService = (DefaultWorkingDirectoryService) lookup( WorkingDirectoryService.class );
-
         workingDirectoryService.setConfigurationService( configurationService );
     }
 
@@ -74,27 +73,13 @@ public class DefaultWorkingDirectoryServiceTest
         throws Exception
     {
         List<Project> projects = new ArrayList<Project>();
-
         Project project = createProject( 7, "org.apache.continuum", "module-a", "1.0-SNAPSHOT",
                                          "scm:local:src/test-projects:flat-multi-module/module-a", true );
-
         projects.add( project );
-
         projects.add( createProject( 8, "org.apache.continuum", "module-b", "1.0-SNAPSHOT",
                                      "scm:local:src/test-projects:flat-multi-module/module-b", true ) );
-
         projects.add( createProject( 6, "org.apache.continuum", "parent-project", "1.0-SNAPSHOT",
                                      "scm:local:src/test-projects:flat-multi-module/parent-project", true ) );
-
-        final File baseWorkingDirectory = new File( getBasedir(), "target" + File.separatorChar + "working-directory" );
-
-        context.checking( new Expectations()
-        {
-            {
-                exactly( 2 ).of( configurationService ).getWorkingDirectory();
-                will( returnValue( baseWorkingDirectory ) );
-            }
-        } );
 
         File projectWorkingDirectory = workingDirectoryService.getWorkingDirectory( project,
                                                                                     "scm:local:src/test-projects:flat-multi-module",
@@ -116,27 +101,13 @@ public class DefaultWorkingDirectoryServiceTest
         throws Exception
     {
         List<Project> projects = new ArrayList<Project>();
-
         Project project = createProject( 10, "org.apache.continuum", "module-a", "1.0-SNAPSHOT",
                                          "scm:local:src/test-projects:regular-multi-module/module-a", true );
-
         projects.add( project );
-
         projects.add( createProject( 11, "org.apache.continuum", "module-b", "1.0-SNAPSHOT",
                                      "scm:local:src/test-projects:regular-multi-module/module-b", true ) );
-
         projects.add( createProject( 9, "org.apache.continuum", "parent-project", "1.0-SNAPSHOT",
                                      "scm:local:src/test-projects:regular-multi-module/", true ) );
-
-        final File baseWorkingDirectory = new File( getBasedir(), "target" + File.separator + "working-directory" );
-
-        context.checking( new Expectations()
-        {
-            {
-                exactly( 2 ).of( configurationService ).getWorkingDirectory();
-                will( returnValue( baseWorkingDirectory ) );
-            }
-        } );
 
         File projectWorkingDirectory = workingDirectoryService.getWorkingDirectory( project,
                                                                                     "scm:local:src/test-projects:regular-multi-module",
@@ -158,14 +129,6 @@ public class DefaultWorkingDirectoryServiceTest
         // test generated path of parent project
         project = createProject( 9, "org.apache.continuum", "parent-project", "1.0-SNAPSHOT",
                                  "scm:local:src/test-projects:regular-multi-module", true );
-
-        context.checking( new Expectations()
-        {
-            {
-                one( configurationService ).getWorkingDirectory();
-                will( returnValue( baseWorkingDirectory ) );
-            }
-        } );
 
         projectWorkingDirectory = workingDirectoryService.getWorkingDirectory( project,
                                                                                "scm:local:src/test-projects:regular-multi-module",
