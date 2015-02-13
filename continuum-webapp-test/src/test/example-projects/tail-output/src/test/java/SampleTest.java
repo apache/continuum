328e2bbd570ecbbf6c2d7e0dacaca8f9439bfeeb
Brent Atkinson <batkinson@apache.org>
2015-02-13 16:32:56 +0000

    Added sample project to test tailing build output.
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@1659592 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-webapp-test/src/test/example-projects/tail-output/src/test/java/SampleTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+   @Before
+   public void setUp() {
+   }
+
+   @Test
+   public void tryOne() {
+   }
+
+   @Test
+   public void tryTwo() {
+   }
+}
