f5e200c3ed10731a2fa6bc203c52fad4a4f62aa0
Brent Atkinson <batkinson@apache.org>
2015-04-29 14:43:34 +0000

    Buffer log appender now properly logs stack traces.
    git-svn-id: https://svn.apache.org/repos/asf/continuum/trunk@1676759 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-webapp/src/main/java/org/apache/continuum/web/util/FixedBufferAppender.java
+++ continuum-webapp/src/main/java/org/apache/continuum/web/util/FixedBufferAppender.java
@@ -2,6 +2,7 @@ package org.apache.continuum.web.util;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -22,8 +23,23 @@ public class FixedBufferAppender
     protected void append( LoggingEvent event )
     {
         String formattedLine = layout.format( event );
+
+        // Add formatted line (may contain throwable if layout handles)
         lines.add( formattedLine );
         queued++;
+
+        // Add throwable information if layout doesn't handle
+        ThrowableInformation throwInfo = event.getThrowableInformation();
+        if ( throwInfo != null && layout.ignoresThrowable() )
+        {
+            for ( String traceLine : throwInfo.getThrowableStrRep() )
+            {
+                lines.add( String.format( "%s%n", traceLine ) );
+                queued++;
+            }
+        }
+
+        // Shrink the queue back to the desired buffer size (temporarily gets larger)
         while ( queued > linesBuffered )
         {
             lines.remove();
